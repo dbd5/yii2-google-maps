b36874186ba0aabcc308a7d4a76b463eb8d3ee4d
ChAngeron <svspretender@gmail.com>
2017-08-26 17:42:10 +0300

    Now can use multiple maps on one page. Can run initialize() for REinitialize all maps

--- views/map.php
+++ views/map.php
@@ -1,13 +1,17 @@
 <div style="width: <?= $this->context->width?>;
     height: <?= $this->context->height?>">
-    <div id="<?= $this->context->mapCanvas?>" style="width:100%; height:100%"></div>
+    <div id="<?= $this->context->mapCanvas?>" data-mapid="<?= $this->context->mapCanvas?>" style="width:100%; height:100%" class="mapid"></div>
 </div>
 <script>
     var map;
     var bounds;
-    function initialize() {
+    if (!window.hasOwnProperty( "initialize_content" )){
+        initialize_content = [];
+    }
+
+    var new_initialize = function initialize_<?= $this->context->mapCanvas?>() {
         var geocoder = new google.maps.Geocoder();
-        window.map = new google.maps.Map(document.getElementById("<?= $this->context->mapCanvas?>"),
+        window.<?= $this->context->mapCanvas?> = new google.maps.Map(document.getElementById("<?= $this->context->mapCanvas?>"),
             {
                 <?php if (!empty($this->context->mapOptions) && is_array($this->context->mapOptions)): ?>
                 <?php foreach ($this->context->mapOptions as $mapOptionKey => $mapOption): ?>
@@ -22,13 +26,13 @@
         <?php if ($this->context->markerFitBounds): ?>
         window.bounds = new google.maps.LatLngBounds();
         <?php elseif (is_array($this->context->center)): ?>
-        window.map.setCenter(new google.maps.LatLng(<?= $this->context->center[0] ?>, <?= $this->context->center[1] ?>));
+        window.<?= $this->context->mapCanvas?>.setCenter(new google.maps.LatLng(<?= $this->context->center[0] ?>, <?= $this->context->center[1] ?>));
         <?php else: ?>
         geocoder.geocode({
             "address": "<?= $this->context->center ?>"
         }, function (results, status) {
             if (status == google.maps.GeocoderStatus.OK) {
-                window.map.setCenter(results[0].geometry.location);
+                window.<?= $this->context->mapCanvas?>.setCenter(results[0].geometry.location);
             }
         });
         <?php endif; ?>
@@ -41,7 +45,7 @@
             <?=$optionKey?>: <?=$option?>,
             <?php endforeach; ?>
             <?php endif; ?>
-            map: window.map
+            map: window.<?= $this->context->mapCanvas?>
         });
         <?php if (isset($marker['title'])): ?>
         marker_<?= $key ?>.setTitle("<?= $marker['title'] ?>");
@@ -51,14 +55,14 @@
             content: '<?= $marker['content'] ?>'
           });
         marker_<?= $key ?>.addListener('click', function() {
-          infowindow_<?= $key ?>.open(window.map, marker_<?= $key ?>);
+          infowindow_<?= $key ?>.open(window.<?= $this->context->mapCanvas?>, marker_<?= $key ?>);
         });
         <?php endif; ?>
         <?php if (is_array($marker['position'])): ?>
         marker_<?= $key ?>.setPosition(new google.maps.LatLng(<?= $marker['position'][0] ?>, <?= $marker['position'][1] ?>));
         <?php if ($this->context->markerFitBounds): ?>
         window.bounds.extend(marker_<?= $key ?>.position);
-        window.map.fitBounds(bounds);
+        window.<?= $this->context->mapCanvas?>.fitBounds(bounds);
         <?php endif; ?>
         <?php else: ?>
         geocoder.geocode({
@@ -68,16 +72,23 @@
                 marker_<?= $key ?>.setPosition(results[0].geometry.location);
                 <?php if ($this->context->markerFitBounds): ?>
                 window.bounds.extend(results[0].geometry.location);
-                window.map.fitBounds(bounds);
+                window.<?= $this->context->mapCanvas?>.fitBounds(bounds);
                 <?php endif; ?>
             }
         });
         <?php endif; ?>
         <?php endforeach; ?>
         <?php endif; ?>
-
-
     }
+
+    initialize_content.push(new_initialize);
+
+    function initialize(){
+        for (var i = 0; i < initialize_content.length; i++) {
+            ( initialize_content[i]() );
+        } 
+    };
+
     function loadScript() {
         var script = document.createElement("script");
         script.type = "text/javascript";
